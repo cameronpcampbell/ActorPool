cab44b120f76c56ee5e1a30561647f5837c26d4f
MightyPart <cameron.campbell325@gmail.com>
2023-04-29 09:17:28 +0100

    Update init.module.lua

--- ActorPool_V3/init.module.lua
+++ ActorPool_V3/init.module.lua
@@ -1,39 +1,49 @@
 --!strict
-local ActorPool = {}; ActorPool.__index = ActorPool
-local ActorPoolInsts = {}; ActorPoolInsts.__index = ActorPoolInsts
-local ActorInsts = {}; ActorInsts.__index = ActorInsts
+local ActorPool = {}
+ActorPool.__index = ActorPool
+local ActorPoolInsts = {}
+ActorPoolInsts.__index = ActorPoolInsts
+local ActorInsts = {}
+ActorInsts.__index = ActorInsts
 
 local Promise = require(script.Promise)
 
-local function createActor(poolBaseActor:Actor, poolFolder:Folder, poolAvailable)
-	local newActor:Actor = poolBaseActor:Clone()
+local function createActor(poolBaseActor: Actor, poolFolder: Folder, poolAvailable)
+	local newActor: Actor = poolBaseActor:Clone()
 	newActor.Parent = poolFolder
 
 	local actorData = setmetatable({
-		actor=newActor, available=poolAvailable,
-		runEvent=newActor.RunEvent, returnEvent=newActor:FindFirstChild("ReturnEvent"),
-		autoPutBack=false, inUse=false, doingWork=false
+		actor = newActor,
+		available = poolAvailable,
+		runEvent = newActor.RunEvent,
+		returnEvent = newActor:FindFirstChild("ReturnEvent"),
+		autoPutBack = false,
+		inUse = false,
+		doingWork = false,
 	}, ActorInsts)
 
 	return actorData
 end
 
-function ActorPool.new(baseActor:Actor, actorsFolder:Folder, amount:number)
-	local pool = setmetatable({baseActor=baseActor, folder=actorsFolder}, ActorPoolInsts)
+function ActorPool.new(baseActor: Actor, actorsFolder: Folder, amount: number)
+	local pool = setmetatable({ baseActor = baseActor, folder = actorsFolder }, ActorPoolInsts)
 
 	local runEvent, returnEvent = baseActor:FindFirstChild("RunEvent"), baseActor:FindFirstChild("ReturnEvent")
-	assert(runEvent, "Your base actor needs a BindableEvent called \"RunEvent\" inside of it!")
+	assert(runEvent, 'Your base actor needs a BindableEvent called "RunEvent" inside of it!')
 
 	local available = table.create(amount)
-	for count = 1,amount do table.insert(available, createActor(baseActor, actorsFolder, available)) end
+	for count = 1, amount do
+		table.insert(available, createActor(baseActor, actorsFolder, available))
+	end
 	pool.available = available
 
 	return pool
 end
 
-function ActorPoolInsts:take(autoPutBack:boolean)
+function ActorPoolInsts:take(autoPutBack: boolean)
 	local actor = table.remove(self.available) or createActor(self.baseActor, self.folder, self.available)
-	actor.autoPutBack = autoPutBack; actor.inUse = true
+	actor.autoPutBack = autoPutBack
+	actor.inUse = true
 	return actor
 end
 
@@ -47,7 +57,8 @@ function ActorInsts:run(...)
 	local data = returnEvent and returnEvent.Event:Wait()
 
 	if self.autoPutBack then
-		self.autoPutBack = false; self.inUse = false
+		self.autoPutBack = false
+		self.inUse = false
 		table.insert(self.available, self)
 	end
 
@@ -61,7 +72,7 @@ function ActorInsts:runPromise(...)
 	self.doingWork = true
 
 	local runEvent, returnEvent = self.runEvent, self.returnEvent
-	local args = {...}
+	local args = { ... }
 
 	return Promise.new(function(resolve, reject, onCancel)
 		runEvent:Fire(table.unpack(args))
@@ -69,7 +80,8 @@ function ActorInsts:runPromise(...)
 		resolve(data)
 	end):finally(function()
 		if self.autoPutBack then
-			self.autoPutBack = false; self.inUse = false
+			self.autoPutBack = false
+			self.inUse = false
 			table.insert(self.available, self)
 		end
 
@@ -78,8 +90,12 @@ function ActorInsts:runPromise(...)
 end
 
 function ActorInsts:putBack()
-	assert(not self.doingWork, "This actor is currently doing work so it may not be put back in the pool at this moment!")
-	self.autoPutBack = false; self.inUse = false
+	assert(
+		not self.doingWork,
+		"This actor is currently doing work so it may not be put back in the pool at this moment!"
+	)
+	self.autoPutBack = false
+	self.inUse = false
 	table.insert(self.available, self)
 end
 
