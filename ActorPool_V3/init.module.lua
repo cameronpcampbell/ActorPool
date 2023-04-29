3e9cabbd8b5e88dfc5741ba1bfe62c359daec5ca
MightyPart <cameron.campbell325@gmail.com>
2023-04-29 09:40:00 +0100

    Update init.module.lua

--- ActorPool_V3/init.module.lua
+++ ActorPool_V3/init.module.lua
@@ -1,26 +1,31 @@
 --!strict
+
 local Pool = {}; Pool.__index = Pool
 local Actor = {}; Actor.__index = Actor
 
 local Promise = require(script.Promise)
 
-local function createActor(poolBaseActor:Actor, poolFolder:Folder, poolAvailable)
-	local newActor:Actor = poolBaseActor:Clone()
+local function createActor(poolBaseActor: Actor, poolFolder: Folder, poolAvailable)
+	local newActor: Actor = poolBaseActor:Clone()
 	newActor.Parent = poolFolder
 
 	local actorData = setmetatable({
-		actor=newActor, available=poolAvailable,
-		runEvent=newActor.RunEvent, returnEvent=newActor:FindFirstChild("ReturnEvent"),
-		autoPutBack=false, inUse=false, doingWork=false
+		actor = newActor,
+		available = poolAvailable,
+		runEvent = newActor.RunEvent,
+		returnEvent = newActor:FindFirstChild("ReturnEvent"),
+		autoPutBack = false,
+		inUse = false,
+		doingWork = false,
 	}, Actor)
 
 	return actorData
 end
 
-
-function Pool:take(autoPutBack:boolean)
+function Pool:take(autoPutBack: boolean)
 	local actor = table.remove(self.available) or createActor(self.baseActor, self.folder, self.available)
-	actor.autoPutBack = autoPutBack; actor.inUse = true
+	actor.autoPutBack = autoPutBack
+	actor.inUse = true
 	return actor
 end
 
@@ -43,34 +48,36 @@ function Actor:runPromise(...)
 	self.doingWork = true
 
 	local runEvent, returnEvent = self.runEvent, self.returnEvent
-	local args = {...}
+	local args = { ... }
 
 	return Promise.new(function(resolve, reject, onCancel)
 		runEvent:Fire(table.unpack(args))
 		local data = returnEvent and returnEvent.Event:Wait()
 		resolve(data)
-		
-	end):finally(function()
+	end)
+	:finally(function()
 		self.doingWork = false
-		
 		if self.autoPutBack then self:putBack() end
 	end)
 end
 
 function Actor:putBack()
 	assert(not self.doingWork, "This actor is currently doing work so it may not be put back in the pool at this moment!")
-	self.autoPutBack = false; self.inUse = false
+	self.autoPutBack = false
+	self.inUse = false
 	table.insert(self.available, self)
 end
 
-return function(baseActor:Actor, actorsFolder:Folder, amount:number)
-	local pool = setmetatable({baseActor=baseActor, folder=actorsFolder}, Pool)
+return function(baseActor: Actor, actorsFolder: Folder, amount: number)
+	local pool = setmetatable({ baseActor = baseActor, folder = actorsFolder }, Pool)
 
 	local runEvent, returnEvent = baseActor:FindFirstChild("RunEvent"), baseActor:FindFirstChild("ReturnEvent")
-	assert(runEvent, "Your base actor needs a BindableEvent called \"RunEvent\" inside of it!")
+	assert(runEvent, 'Your base actor needs a BindableEvent called "RunEvent" inside of it!')
 
 	local available = table.create(amount)
-	for count = 1,amount do table.insert(available, createActor(baseActor, actorsFolder, available)) end
+	for count = 1, amount do
+		table.insert(available, createActor(baseActor, actorsFolder, available))
+	end
 	pool.available = available
 
 	return pool
